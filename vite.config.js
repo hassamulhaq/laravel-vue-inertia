f159d25a50e6ce55520c586689ba293685eb1cdd
hassamulhaq <developer.hassam@gmail.com>
2023-01-07 00:17:54 +0500

    Setup VUE template

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
+import process from "@inertiajs/inertia-vue3/.eslintrc";
 
 export default defineConfig({
+    define: {
+        'process.env': process.env
+    },
     plugins: [
         laravel({
             input: 'resources/js/app.js',
