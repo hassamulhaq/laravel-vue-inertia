9250a90beef56149fa1cd4d56f540bdbfe523166
hassamulhaq <developer.hassam@gmail.com>
2023-01-19 23:59:37 +0500

    Invoice -mfcs created

---
+++ app/Http/Controllers/Invoice/InvoicesController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers\Invoice;
+
+use App\Http\Controllers\Controller;
+use App\Models\Invoice\Invoice;
+use Illuminate\Http\Request;
+
+class InvoicesController extends Controller
+{
+    public function index()
+    {
+
+    }
+
+    public function create()
+    {
+    }
+
+    public function store(Request $request)
+    {
+    }
+
+    public function show(Invoice $invoice)
+    {
+    }
+
+    public function edit(Invoice $invoice)
+    {
+    }
+
+    public function update(Request $request, Invoice $invoice)
+    {
+    }
+
+    public function destroy(Invoice $invoice)
+    {
+    }
+}
---
+++ app/Models/Invoice/Invoice.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Models\Invoice;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Invoice extends Model
+{
+}
