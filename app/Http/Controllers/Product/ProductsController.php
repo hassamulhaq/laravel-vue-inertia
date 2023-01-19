e64a6fa54fe5b14fa5dd91d4b27c49a1e8ff6bda
hassamulhaq <developer.hassam@gmail.com>
2023-01-19 23:58:44 +0500

    Product -mfcs created

---
+++ app/Http/Controllers/Product/ProductsController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers\Product;
+
+use App\Http\Controllers\Controller;
+use App\Models\Product\Product;
+use Illuminate\Http\Request;
+
+class ProductsController extends Controller
+{
+    public function index()
+    {
+
+    }
+
+    public function create()
+    {
+    }
+
+    public function store(Request $request)
+    {
+    }
+
+    public function show(Product $product)
+    {
+    }
+
+    public function edit(Product $product)
+    {
+    }
+
+    public function update(Request $request, Product $product)
+    {
+    }
+
+    public function destroy(Product $product)
+    {
+    }
+}
---
+++ app/Models/Product/Product.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Models\Product;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+}
